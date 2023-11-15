fd655696188e2ce7401fed834e9abfc2185c1c54
Sergeev Mikhail <sergeev-michael@mail.ru>
2023-11-15 09:22:14 +0600

    "Set Bootstrap for using css, made import Bootstrap. After delete dir assets, App.css, index.css. Create .editorconfig, and rule 'semi':'error' on .eslintrc. Create repository and project on GitHub "

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Toolbar from './components/Toolbar/Toolbar.tsx';
+
+function App() {
+  return (
+    <>
+      <header>
+        <Toolbar />
+      </header>
+      <main>
+
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Toolbar/Toolbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Toolbar:React.FC = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-primary-subtle">
+      <div className="container-fluid">
+        <span className="navbar-brand mt-0 mb-0 me-auto ms-5">Home work 59</span>
+      </div>
+    </nav>
+  );
+};
+
+export default Toolbar;
